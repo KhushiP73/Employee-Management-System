a23d69df884a32f4043ec52987d993d98aff270c
Khushi Panchal <thecodingjourney007@gmail.com>
2026-08-05 17:11:45 +0530

    Implement view employees with department details

--- src/employee.py
+++ src/employee.py
@@ -28,7 +28,7 @@ def add_employee():
         VALUES
         (%s, %s, %s, %s, %s, %s, %s)
         """
-        
+
         cur.execute(
             query,
             (
@@ -48,6 +48,60 @@ def add_employee():
 
     except Exception as e:
         print("\nError:", e)
+
+    finally:
+        if "cur" in locals():
+            cur.close()
+
+        if "conn" in locals():
+            conn.close()
+
+def view_employees():
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+
+        query = """
+        SELECT
+            e.employee_id,
+            e.first_name,
+            e.last_name,
+            d.department_name,
+            e.email,
+            e.phone,
+            e.hire_date,
+            e.salary
+        FROM employees e
+        JOIN departments d
+            ON e.department_id = d.department_id
+        ORDER BY e.employee_id;
+        """
+
+        cur.execute(query)
+        employees = cur.fetchall()
+
+        if not employees:
+            print("\nNo employees found.")
+            return
+
+        print("\n================ Employee List ================\n")
+        print(
+            f"{'ID':<5}"
+            f"{'Name':<25}"
+            f"{'Department':<20}"
+            f"{'Salary':<12}"
+        )
+        print("-" * 65)
+        for emp in employees:
+            print(
+                f"{emp[0]:<5}"
+                f"{emp[1] + ' ' + emp[2]:<25}"
+                f"{emp[3]:<20}"
+                f"{emp[7]:<12}"
+            )
+
+    except Exception as e:
+        print("Error:", e)
 
     finally:
         if "cur" in locals():
--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-from employee import add_employee
+from employee import add_employee, view_employees
 
 def main():
     add_employee()
+    view_employees()
 
 if __name__ == "__main__":
     main()
